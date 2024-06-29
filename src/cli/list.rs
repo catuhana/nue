c63231a1dd4c8db049748c8b8b3836cc8ce0f563
tuhana <tuhana.cat+git@gmail.com>
2024-06-29 13:39:03 +0300

    Generate a better tree for list command

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -17,13 +17,56 @@ impl NueCommand for CommandArguments {
                 .into_json()?;
 
         let releases = response
-            .iter()
-            .map(|release| release.version.to_string())
-            .collect::<Vec<String>>()
-            .join("\n");
+            .into_iter()
+            .map(|release| release)
+            .collect::<Vec<types::node::Release>>();
 
-        println!("{releases}");
+        println!("{}", print_version_tree(&releases));
 
         Ok(())
     }
+}
+
+fn print_version_tree(releases: &[types::node::Release]) -> String {
+    let mut tree_string = String::new();
+
+    let mut current_major = None;
+    let mut current_minor = None;
+    let mut current_patch_written = false;
+
+    for release in releases {
+        if current_major != Some(release.version.major) {
+            current_major = Some(release.version.major);
+            current_minor = None;
+            current_patch_written = false;
+
+            tree_string.push_str(&format!(
+                "v{}{}\n",
+                release.version.major,
+                match &release.lts {
+                    types::node::LTS::CodeName(code_name) => format!(" (LTS, {})", code_name),
+                    types::node::LTS::Bool(_false) => "".to_string(),
+                }
+            ));
+        }
+
+        if current_minor != Some(release.version.minor) || !current_patch_written {
+            current_minor = Some(release.version.minor);
+            current_patch_written = true;
+
+            tree_string.push_str(&format!(
+                "  - v{}.{}\n",
+                release.version.major, release.version.minor
+            ));
+        }
+
+        if release.version.patch > 0 {
+            tree_string.push_str(&format!(
+                "    - v{}.{}.{}\n",
+                release.version.major, release.version.minor, release.version.patch
+            ));
+        }
+    }
+
+    tree_string
 }
