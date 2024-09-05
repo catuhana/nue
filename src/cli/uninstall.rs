bad45af19f7dfcf1cb2a6ca068eec31bf178bc50
tuhana <tuhana.cat+git@gmail.com>
2024-09-06 01:12:07 +0300

    Tell the exact file that the sourcing happens for more information in `uninstall`

--- src/cli/uninstall.rs
+++ src/cli/uninstall.rs
@@ -19,9 +19,35 @@ impl NueCommand for CommandArguments {
         println!("Node uninstalled successfully.");
 
         if utils::check::path_contains(".nue/node/bin")? {
-            println!("Node is still on your PATH. Please remove the environment file from your shell configuration.");
+            println!("Node is still in your `PATH`. Remove the sourced env script from your shell profile ({}).", files_in_home_containing("$HOME/.nue/env").await?.join(", "));
         }
 
         Ok(())
     }
+}
+
+async fn files_in_home_containing(substring: &str) -> anyhow::Result<Vec<String>> {
+    let home_dir =
+        dirs::home_dir().ok_or_else(|| anyhow::anyhow!("failed to get home directory"))?;
+    let mut matching_files = Vec::new();
+
+    let mut dir = fs::read_dir(home_dir).await?;
+    while let Some(entry) = dir.next_entry().await? {
+        let path = entry.path();
+        if path.is_dir() {
+            continue;
+        }
+
+        match fs::read_to_string(&path).await {
+            Ok(contents) if contents.contains(substring) => {
+                matching_files.push(format!(
+                    "~/{}",
+                    path.file_name().unwrap().to_string_lossy().to_string()
+                ));
+            }
+            _ => {}
+        }
+    }
+
+    Ok(matching_files)
 }
